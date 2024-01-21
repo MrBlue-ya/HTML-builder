b1f34a542b2ea8250e305bfad492c7c1fbf9d434
Mr.Blue <sergey.gritskevich98@mail.ru>
2024-01-21 19:01:49 +0300

    feat: add feature that create folder in 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,34 +1,59 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+const newFolder = path.join(__dirname, 'files-copy');
+
+fs.readdir(__dirname, (err, files) => {
   if (err) {
     console.error(err);
     return;
+  }
+  if (files.includes('files-copy')) {
+    console.log('Folder "files-copy" has already been created!');
+    copyDir();
   } else {
-    console.log('folder "files" contain:');
-    console.log(files);
-
-    for (let file of files) {
-      fs.copyFile(
-        path.join(__dirname, 'files', file),
-        path.join(__dirname, 'files-copy', file),
-        (err) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-        },
-      );
-    }
-    console.log('copied files in "files-copy": ');
-    fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+    fs.mkdir(newFolder, (err) => {
       if (err) {
         console.error(err);
         return;
       } else {
-        console.log(files);
+        console.log('folder was successfully create');
       }
     });
+    copyDir();
   }
 });
+
+function copyDir() {
+  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
+    } else {
+      console.log('folder "files" contain:');
+      console.log(files);
+
+      for (let file of files) {
+        fs.copyFile(
+          path.join(__dirname, 'files', file),
+          path.join(__dirname, 'files-copy', file),
+          (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+          },
+        );
+      }
+      console.log('copied files in "files-copy": ');
+      fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+        if (err) {
+          console.error(err);
+          return;
+        } else {
+          console.log(files);
+        }
+      });
+    }
+  });
+}
