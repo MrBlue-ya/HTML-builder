7e351f9cd6ef7c6b2af09633bc06be6059bb3f27
Mr.Blue <sergey.gritskevich98@mail.ru>
2024-01-24 20:13:31 +0300

    refactor: update files-copy folder when occur copy files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,7 +1,8 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-const newFolder = path.join(__dirname, 'files-copy');
+const filesP = path.join(__dirname, 'files');
+const filesCopyP = path.join(__dirname, 'files-copy');
 
 fs.readdir(__dirname, (err, files) => {
   if (err) {
@@ -9,51 +10,55 @@ fs.readdir(__dirname, (err, files) => {
     return;
   }
   if (files.includes('files-copy')) {
-    console.log('Folder "files-copy" has already been created!');
+    console.log('Folder "files-copy" has already been created');
     copyDir();
   } else {
-    fs.mkdir(newFolder, (err) => {
+    fs.mkdir(filesCopyP, (err) => {
       if (err) {
         console.error(err);
         return;
       } else {
         console.log('folder was successfully create');
       }
+      copyDir();
     });
-    copyDir();
   }
 });
 
 function copyDir() {
-  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+  fs.rm(filesCopyP, { recursive: true }, (err) => {
     if (err) {
-      console.error(err);
+      console.error('Failed to clear directory:', err);
       return;
-    } else {
-      console.log('folder "files" contain:');
-      console.log(files);
-
-      for (let file of files) {
-        fs.copyFile(
-          path.join(__dirname, 'files', file),
-          path.join(__dirname, 'files-copy', file),
-          (err) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
-          },
-        );
-      }
-      console.log('copied files in "files-copy": ');
-      fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
-        if (err) {
-          console.error(err);
-          return;
-        } else {
-          console.log(files);
-        }
-      });
     }
+    fs.mkdir(filesCopyP, (err) => {
+      if (err) {
+        console.error('Failed to create directory:', err);
+        return;
+      }
+    });
+    fs.readdir(filesP, (err, files) => {
+      if (err) {
+        console.error(err);
+        return;
+      } else {
+        console.log('folder "files" contain:');
+        console.log(files);
+
+        for (let file of files) {
+          fs.copyFile(
+            path.join(filesP, file),
+            path.join(filesCopyP, file),
+            (err) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+            },
+          );
+        }
+        console.log('all files have been copied');
+      }
+    });
   });
 }
