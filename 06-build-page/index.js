c9b93c0874ba3aa2446f4945b8876d085c8f033f
Mr.Blue <sergey.gritskevich98@mail.ru>
2024-01-24 22:02:19 +0300

    feat: add oppotunity to add new tag about

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,6 +1,53 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
+const testFilesPath = path.join(__dirname, 'test-files');
+
+fs.readFile(
+  path.join(__dirname, 'template.html'),
+  { encoding: 'utf-8' },
+  (err, file) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    if (file.includes('{{about}}')) {
+      fs.copyFile(
+        path.join(testFilesPath, 'components', 'about.html'),
+        path.join(__dirname, 'components', 'about.html'),
+        (err) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+        },
+      );
+
+      fs.copyFile(
+        path.join(testFilesPath, 'styles', '04-about.css'),
+        path.join(__dirname, 'styles', '04-about.css'),
+        (err) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+        },
+      );
+
+      fs.copyFile(
+        path.join(testFilesPath, 'images', 'squirrel-2.jpg'),
+        path.join(__dirname, 'assets', 'img', 'squirrel-2.jpg'),
+        (err) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+        },
+      );
+    }
+  },
+);
+
 // check if dir have folder 'project-dist'
 // if empty - create folder
 fs.readdir(__dirname, (err, files) => {
@@ -64,24 +111,39 @@ function compileHtml() {
                   }
 
                   fs.readFile(
-                    path.join(componentsHtml, 'header.html'),
+                    path.join(componentsHtml, 'about.html'),
                     { encoding: 'utf-8' },
-                    (err, headerHtml) => {
+                    (err, aboutHtml) => {
                       if (err) {
                         console.log(err);
                         return;
                       }
 
-                      const updateindexHtml = indexHtml
-                        .replace('{{header}}', headerHtml)
-                        .replace('{{articles}}', articlesHtml)
-                        .replace('{{footer}}', footerHtml);
+                      fs.readFile(
+                        path.join(componentsHtml, 'header.html'),
+                        { encoding: 'utf-8' },
+                        (err, headerHtml) => {
+                          if (err) {
+                            console.log(err);
+                            return;
+                          }
+                          const updateindexHtml = indexHtml
+                            .replace('{{header}}', headerHtml)
+                            .replace('{{about}}', aboutHtml)
+                            .replace('{{articles}}', articlesHtml)
+                            .replace('{{footer}}', footerHtml);
 
-                      fs.writeFile(pathIndexHtml, updateindexHtml, (err) => {
-                        if (err) {
-                          console.error(err);
-                        }
-                      });
+                          fs.writeFile(
+                            pathIndexHtml,
+                            updateindexHtml,
+                            (err) => {
+                              if (err) {
+                                console.error(err);
+                              }
+                            },
+                          );
+                        },
+                      );
                     },
                   );
                 },
