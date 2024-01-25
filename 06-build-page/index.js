563da04123b892c34015e9fc2db8cef78e465c2e
Mr.Blue <sergey.gritskevich98@mail.ru>
2024-01-25 19:16:43 +0300

    refactor: 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,53 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
 
-const testFilesPath = path.join(__dirname, 'test-files');
-
-fs.readFile(
-  path.join(__dirname, 'template.html'),
-  { encoding: 'utf-8' },
-  (err, file) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    if (file.includes('{{about}}')) {
-      fs.copyFile(
-        path.join(testFilesPath, 'components', 'about.html'),
-        path.join(__dirname, 'components', 'about.html'),
-        (err) => {
-          if (err) {
-            console.log(err);
-            return;
-          }
-        },
-      );
-
-      fs.copyFile(
-        path.join(testFilesPath, 'styles', '04-about.css'),
-        path.join(__dirname, 'styles', '04-about.css'),
-        (err) => {
-          if (err) {
-            console.log(err);
-            return;
-          }
-        },
-      );
-
-      fs.copyFile(
-        path.join(testFilesPath, 'images', 'squirrel-2.jpg'),
-        path.join(__dirname, 'assets', 'img', 'squirrel-2.jpg'),
-        (err) => {
-          if (err) {
-            console.log(err);
-            return;
-          }
-        },
-      );
-    }
-  },
-);
-
 // check if dir have folder 'project-dist'
 // if empty - create folder
 fs.readdir(__dirname, (err, files) => {
@@ -76,82 +29,46 @@ fs.readdir(__dirname, (err, files) => {
 // Copy html files from folder components to project-dist
 function compileHtml() {
   const pathIndexHtml = path.join(__dirname, 'project-dist', 'index.html');
+  const componentsHtml = path.join(__dirname, 'components');
 
   fs.copyFile(path.join(__dirname, 'template.html'), pathIndexHtml, (err) => {
     if (err) {
       console.error(err);
       return;
     }
-    const componentsHtml = path.join(__dirname, 'components');
-    fs.readFile(
-      path.join(__dirname, 'project-dist', 'index.html'),
-      { encoding: 'utf-8' },
-      (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          const indexHtml = data;
 
-          fs.readFile(
-            path.join(componentsHtml, 'articles.html'),
-            { encoding: 'utf-8' },
-            (err, articlesHtml) => {
+    fs.readFile(pathIndexHtml, 'utf8', (err, indexData) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      fs.readdir(componentsHtml, 'utf8', (err, files) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+
+        for (let file of files) {
+          let fileName = file.split('.').slice(0, 1).join('');
+
+          fs.readFile(path.join(componentsHtml, file), 'utf8', (err, data) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            indexData = indexData.replace(`{{${fileName}}}`, data);
+
+            // Запись обновленного содержимого обратно в файл
+            fs.writeFile(pathIndexHtml, indexData, 'utf8', (err) => {
               if (err) {
                 console.log(err);
-                return;
               }
-
-              fs.readFile(
-                path.join(componentsHtml, 'footer.html'),
-                { encoding: 'utf-8' },
-                (err, footerHtml) => {
-                  if (err) {
-                    console.log(err);
-                    return;
-                  }
-
-                  fs.readFile(
-                    path.join(componentsHtml, 'about.html'),
-                    { encoding: 'utf-8' },
-                    (err, aboutHtml) => {
-                      if (err) {
-                        return;
-                      }
-
-                      fs.readFile(
-                        path.join(componentsHtml, 'header.html'),
-                        { encoding: 'utf-8' },
-                        (err, headerHtml) => {
-                          if (err) {
-                            console.log(err);
-                            return;
-                          }
-                          const updateindexHtml = indexHtml
-                            .replace('{{header}}', headerHtml)
-                            .replace('{{about}}', aboutHtml)
-                            .replace('{{articles}}', articlesHtml)
-                            .replace('{{footer}}', footerHtml);
-
-                          fs.writeFile(
-                            pathIndexHtml,
-                            updateindexHtml,
-                            (err) => {
-                              if (err) {
-                                console.error(err);
-                              }
-                            },
-                          );
-                        },
-                      );
-                    },
-                  );
-                },
-              );
-            },
-          );
+            });
+          });
         }
-      },
-    );
+      });
+    });
   });
 }
 
